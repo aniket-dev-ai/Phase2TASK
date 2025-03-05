0cc06c803b1af14dc82c2f1c6e773229938bff0d
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-03-05 18:06:26 +0530

    done

--- Frontend/src/component/DeleteButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DeleteButton = ({ onClick }) => {
-  return <button className='bg-red-500 text-white' onClick={onClick}>Delete</button>;
-};
-
-export default DeleteButton;
--- Frontend/src/context/TodoContext.jsx
+++
@@ -1,7 +0,0 @@
-import React, { createContext } from "react";
-
-const initialState = {
-  todos: [],
-};
-
-export const TodoContext = createContext(initialState);
